87d834c2813fa149b424ff84074577d401179735
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-03 10:47:38 -0700

    wrote test for retriving list of books

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,20 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+// const { books } = require('../data/books');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/books should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    const expected = books.map((book) => {
+      return { id: book.id, title: book.title, released: book.released };
+    });
+    expect(res.body).toEqual(expected);
   });
   afterAll(() => {
     pool.end();
