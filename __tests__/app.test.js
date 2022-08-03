3e5246df74bf6964d326bc00991e4b9d3988a69d
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-03 16:52:12 -0700

    test for getting single book by id passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,10 +18,10 @@ describe('backend-express-template routes', () => {
   it('/books/:id returns a single book and author info', async () => {
     const res = await request(app).get('/books/1');
     const book1 = {
-      id: 1,
+      id: '1',
       title: 'Eat Pray Love',
       author_name: 'Elizabeth Gilbert',
-      released: 2006,
+      released: '2006',
     };
     expect(res.body).toEqual(book1);
   });
