428e43f5639bfdeed7df04012697a0b5d168ca6a
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-03 16:25:14 -0700

    wrote test for getting individual book

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,6 +15,16 @@ describe('backend-express-template routes', () => {
     const res = await request(app).get('/authors');
     expect(res.body.length).toEqual(3);
   });
+  it('/books/:id returns a single book and author info', async () => {
+    const res = await request(app).get('/books/1');
+    const book1 = {
+      id: 1,
+      title: 'Eat Pray Love',
+      author_name: 'Elizabeth Gilbert',
+      released: 2006,
+    };
+    expect(res.body).toEqual(book1);
+  });
 });
 afterAll(() => {
   pool.end();
